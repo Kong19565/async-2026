ff888734229f2e6af1d172defe26296e7fa31156
Kong19565 <khaogong48@gmail.com>
2026-06-17 10:07:01 +0700

    Create process in function calculate_ticket_price

--- WX/ticket.py
+++ WX/ticket.py
@@ -1,9 +1,17 @@
 import sys
 
+# Age less than 12 (age < 12): 120 Baht
+# Age between 12 and 60 (12 <= age <= 60): 200 Baht
+# Age over 60 (age > 60): 150 Baht
+
 def calculate_ticket_price(age):
-    # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
-    pass
-    # --------------------------------------------------------
+    if age < 12:
+        return 120
+    elif 12 <= age <= 60:
+        return 200
+    else:
+        return 150
+
 
 def main():
     # เปลี่ยนมาเช็ก > 1 และใช้ sys.argv[-1] เพื่อความแม่นยำใน VPL
